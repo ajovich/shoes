0e5edb997f10dfb0256c4852594a69b65a2f47ae
Allysha Jovich <ajovich@hotmail.com>
2021-09-01 22:35:00 -0700

    updated signup page, fixed bug, user is now able to successfully signup and login

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import Auth from '../utils/auth';
 import { ADD_USER } from '../utils/mutations';
-import { Grid, Paper, Avatar, TextField, Button, Typography, FormGroup } from '@material-ui/core';
+import { Grid, Paper, Avatar, TextField, Button, Typography } from '@material-ui/core';
 
 function Signup(props) {
   const [formState, setFormState] = useState({ email: '', password: '' });
@@ -31,12 +31,12 @@ function Signup(props) {
     });
   };
 
-  // const paperOutlinedStyle={padding :20, height: "55vh", width: 400, margin: "100px auto"}
-  // const avatarCircleStyle={backgroundColor: "#D5D69A"}
-  // const buttonStyle={margin: "24px 0", backgroundColor: "#D5D69A", fontSize: "18px", color: "#141414"}
-  // const signUpLinkStyle={marginLeft: "8px", fontSize: "12px"}
-  // const noAccountStyle={fontSize: "12px"}
-  // const gridStyle={backgroundColor: "#FBFBF2", height: "100vh"}
+  const paperOutlinedStyle={padding :20, height: "55vh", width: 400, margin: "100px auto"}
+  const avatarCircleStyle={backgroundColor: "#D5D69A"}
+  const buttonStyle={margin: "24px 0", backgroundColor: "#D5D69A", fontSize: "18px", color: "#141414"}
+  const signUpLinkStyle={marginLeft: "8px", fontSize: "12px"}
+  const noAccountStyle={fontSize: "12px"}
+  const gridStyle={backgroundColor: "#FBFBF2", height: "100vh"}
 
   return (
     // <Grid style={gridStyle}>  
@@ -66,56 +66,32 @@ function Signup(props) {
     //       </Typography>
     //   </Paper>
     // </Grid>
-    <div className="container my-1">
-      <Link to="/login">← Go to Login</Link>
+    <Grid style={gridStyle}>
+        <Paper variant="outlined" style={paperOutlinedStyle}>
+            <Grid align="center">
+                <br />
+                <Avatar style={avatarCircleStyle}></Avatar>
+                <br />
+                <h2>Sign Up For Free!</h2>
+                <br />
+            </Grid>
 
-      <h2>Signup</h2>
       <form onSubmit={handleFormSubmit}>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="firstName">First Name:</label>
-          <input
-            placeholder="First"
-            name="firstName"
-            type="firstName"
-            id="firstName"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="lastName">Last Name:</label>
-          <input
-            placeholder="Last"
-            name="lastName"
-            type="lastName"
-            id="lastName"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="email">Email:</label>
-          <input
-            placeholder="youremail@test.com"
-            name="email"
-            type="email"
-            id="email"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="pwd">Password:</label>
-          <input
-            placeholder="******"
-            name="password"
-            type="password"
-            id="pwd"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row flex-end">
-          <button type="submit">Submit</button>
-        </div>
+          <TextField label="First Name" placeholder="First name" fullWidth name="firstName" type="firstName" id="firstName" onChange={handleChange} />
+          <TextField label="Last Name" placeholder="Last name" fullWidth name="lastName" type="lastName" id="lastName" onChange={handleChange} />
+          <TextField label="Email" placeholder="Email address" fullWidth name="email" type="email" id="email" onChange={handleChange} />
+          <TextField label="Password" placeholder="Password" fullWidth name="password" type="password" id="pwd" onChange={handleChange} />
+
+          <Button type="submit" color="default" variant="contained" style={buttonStyle} fullWidth>SIGN UP</Button>
       </form>
-    </div>
+
+          <Typography style={noAccountStyle}> Already have an account?
+              <Link to="/login" style={signUpLinkStyle}>
+                  Login
+              </Link>
+          </Typography>
+      </Paper>
+    </Grid>
   );
 }
 
